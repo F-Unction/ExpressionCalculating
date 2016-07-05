20b672b9d8c7dd8959a25ff04330a648a7938d20
Liu Mingyang <e.d.boy@aliyun.com>
2016-07-05 20:39:49 +0800

    Add files via upload

---
+++ ExpressionCalculating.cpp
@@ -0,0 +1,45 @@
+// ExpressionCalculating.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "main.h"
+#include <iostream>
+
+
+string qu(string x)
+{
+	return "\"" + string(x) + "\" ";
+}
+#define q(x) qu(#x)
+
+int main()
+{
+	int a; char c; float f;
+	scanf_s("%d %c %f", &a, &c, &f);
+
+
+loop:cout << "Please input a expression" << endl \
+	<< "(input \"help\" to get help,input\"exit\" to exit):" << endl;
+	string exp;
+	getline(cin,exp);
+
+	if (exp == "exit")goto endLab;
+	if (exp == "help")goto helpLab;
+
+	double result;
+	if(expCalculate(exp, result))
+		cout << "The result:" << result << endl << endl;
+	else cout << "Sorry , your expression is wrong." << endl << endl;
+
+	goto loop;
+
+helpLab:cout << "It has supported operators:" << q(+) + q(-) + q(*) + q(/ ) + q(^) + q(%) + qu("(") + qu(")") << endl;
+	cout << "   functions:" << q(sin) + q(cos) + q(tan) + q(arcsin) + q(arccos) + q(arctan) + \
+		q(lg) + q(ln) + q(log) + q(abs) + q(int) + q(sqrt) + q(rand) << endl;
+	cout << "   numbers:" << q(PI) << q(E) << endl << endl;
+	goto loop;
+
+	endLab : return 0;
+}
+
